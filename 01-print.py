80eeef549aa26566cac997256af48aba7846a0ef
veeranki2014 <veeranki2014@gmail.com>
2023-09-23 13:24:26 -0500

    terraform vpc peering

---
+++ 01-print.py
@@ -0,0 +1,7 @@
+i = 1
+x = 3
+sum = 0
+while ( i <= x ):
+    sum += i
+    i += 1
+print(sum)
---
+++ 03-while.py
@@ -0,0 +1,15 @@
+# i = 1
+# x = 3
+# sum = 0
+# while ( i <= x ):
+#     sum += i
+#     i += 1
+# print(sum)
+
+i = 1
+x = 3
+sum = 0
+while ( i <= 3 ):
+    sum += i
+    i += 1
+print(sum)
